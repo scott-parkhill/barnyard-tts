385804c6436ea9e6e67d9ed18967ffb8ee04823b
Scott Parkhill <scott.parkhill@gmail.com>
2025-07-29 11:25:35 -0400

    Add api.py

---
+++ api.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI, HTTPException, Query
+from fastapi.responses import StreamingResponse
+from tts_cli import synthesize
+
+app = FastAPI()
+
+@app.get("/synthesize")
+def synthesize_api(
+    text: str = Query(...),
+    language_id: int = Query(...),
+    speaker_id: int = Query(...)
+):
+    try:
+        buffer = synthesize(text,
+                                 tts_model="../models/multilingual_fnet_last.ckpt",
+                                 vocoder="vocos_last.ckpt",
+                                 vocos_config="vocos-matcha.yaml",
+                                 language_id=language_id,
+                                 speaker_id=speaker_id)
+        
+        return StreamingResponse(buffer, media_type="audio/wav")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
